da43a8c791f5df2bdd82721668da207789438573
cagregoris <carolyn.a.scobie@gmail.com>
2022-08-16 13:51:50 -0600

    routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom";
+
+//COMPONENTS
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
 
 function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
    <Fragment>
-
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route exact path="/login" element={ !isAuthenticated ?  (<Login/>) : (< Navigate to="/dashboard" />) } />
+            <Route exact path="/register" element={ !isAuthenticated ? (<Register/>) : (< Navigate to="/login" />) } />
+            <Route exact path="/dashboard" element={ isAuthenticated ? (<Dashboard/>) : (< Navigate to="/login" />) } />
+          </Routes>
+        </div>
+      </Router>
    </Fragment>
   );
 }
---
+++ client/src/components/Login.js
@@ -0,0 +1,11 @@
+import React, {Fragment} from 'react'
+
+function Login() {
+  return (
+    <Fragment>
+      <h1>Login</h1>
+    </Fragment>
+  )
+}
+
+export default Login
